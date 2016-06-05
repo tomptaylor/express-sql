92973f6630ed05d305ca470e9401142d62fdc2f4
Tom Taylor <tomptaylor@gmail.com>
2016-06-04 21:52:19 -0600

    got it working2

--- schema/personsSchema.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var Schema = mongoose.Schema;
-var PersonSchema = new Schema({
-    name: String,
-
-});
-var Person = mongoose.model('Persons', PersonSchema);
-module.exports = Person;
--- schema/placesSchema.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var Schema = mongoose.Schema;
-var PlacesSchema = new Schema({
-    name: String,
-
-});
-var Places = mongoose.model('Places', PlacesSchema);
-module.exports = Places;
