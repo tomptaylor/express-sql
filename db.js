42446ec1652eb43dc52070b3865fc7dad6c89a08
Tom Taylor <tomptaylor@gmail.com>
2016-06-27 19:25:32 -0600

    Yeah, got this to work

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "denver123",
+  password: "*******",
   database: "mydb",
   multipleStatements: true
 });
